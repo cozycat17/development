ad522ae95de8ddb5c9c80068b3c3b15bfd2d5520
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 <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>
2022-11-19 11:03:23 -0500

    5: Added dropdown menu

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -1,8 +1,19 @@
+import "./FilterBar.css";
+import { DownOutlined } from "@ant-design/icons";
+import { Dropdown, Space } from "antd";
+import { useState } from "react";
+
 function FilterBar(props) {
+  const [sortMethod, setSortMethod] = useState("");
   const sortByLife = (opposite) => {
     var data = [...props.animals];
     data.sort((a, b) => opposite * (a.lifespan - b.lifespan));
     props.setAnimals(data);
+    setSortMethod(
+      opposite < 0
+        ? "Life expectancy: high to low"
+        : "Life expectancy: low to high"
+    );
   };
 
   const sortByWeight = (opposite) => {
@@ -11,14 +22,42 @@ function FilterBar(props) {
       (a, b) => opposite * (parseFloat(a.weight_max) - parseFloat(b.weight_max))
     );
     props.setAnimals(data);
+    setSortMethod(opposite < 0 ? "Weight: high to low" : "Weight: low to high");
   };
 
+  const items = [
+    {
+      label: <div onClick={() => sortByWeight(1)}> Weight: low to high</div>,
+      key: "0",
+    },
+    {
+      label: <div onClick={() => sortByWeight(-1)}>Weight: high to low</div>,
+      key: "1",
+    },
+    {
+      label: (
+        <div onClick={() => sortByLife(1)}>Life expectancy: low to high</div>
+      ),
+      key: "3",
+    },
+    {
+      label: (
+        <div onClick={() => sortByLife(-1)}>Life expectancy: high to low</div>
+      ),
+      key: "4",
+    },
+  ];
+
   return (
-    <div>
-      <h1 onClick={() => sortByWeight(1)}>Sort by weight_max</h1>
-      <h1 onClick={() => sortByWeight(-1)}>rev by weight_max</h1>
-      <h1 onClick={() => sortByLife(1)}>Sort by life</h1>
-      <h1 onClick={() => sortByLife(-1)}>rev by life</h1>
+    <div className="filterBarContainer">
+      <Dropdown menu={{ items }} trigger={["click"]}>
+        <a onClick={(e) => e.preventDefault()}>
+          <Space>
+            sort by: {sortMethod}
+            <DownOutlined />
+          </Space>
+        </a>
+      </Dropdown>
     </div>
   );
 }
